6bc84bf6241b93a2e938ccf54a4fc2f90d30ddb2
Andrei Deak <andrei.deak@outlook.com>
2020-09-15 07:42:16 +0300

    fixed sass conversion into css

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,30 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// Hard code this to production but can be adapted to accept args to change env.
+const mode = 'production';
 
 module.exports = {
+
+    mode,
+
     entry: './src/js/app.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: "/dist/css"
+    },
+    resolve: {
+      extensions: ['.css', '.scss'],
+      alias: {
+        // Provides ability to include node_modules with ~
+        '~': path.resolve(process.cwd(), 'src'),
+      },
+    },
+    entry: {
+      // Will create "styles.css" in "css" dir.
+      "styles": './src/sass/main.scss',
     },
     module: {
     rules: [
@@ -23,6 +42,17 @@ module.exports = {
             loader: "html-loader"
           }
         ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          // Extract and save the final CSS.
+          MiniCssExtractPlugin.loader,
+          // Load the CSS, set url = false to prevent following urls to fonts and images.
+          { loader: "css-loader", options: { url: false, importLoaders: 1 } },
+          // Load the SCSS/SASS
+          { loader: 'sass-loader' },
+        ],
       }
     ]
   },
@@ -30,6 +60,11 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
+    }),
+    // Define the filename pattern for CSS.
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
     })
   ]
 };
